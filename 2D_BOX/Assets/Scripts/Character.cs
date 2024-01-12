0f538989a980fa83d5df4ce136fd8a5ea8c4b868
rchellee <ruerasritchelle07@gmail.com>
2024-01-12 21:33:07 +0800

    update char selection
    added icons

---
+++ 2D_BOX/Assets/Scripts/Character.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Character
+{
+   public GameObject prefab;
+   public string name;
+   public Sprite icon;
+
+}
