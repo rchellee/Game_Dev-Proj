cd911aa00fcba398d7f30e14640a24890604cb80
rchellee <ruerasritchelle07@gmail.com>
2024-01-13 18:45:54 +0800

    Update Loading.cs
    done with loading

--- 2D_BOX/Assets/Scripts/Loading.cs
+++ 2D_BOX/Assets/Scripts/Loading.cs
@@ -24,6 +24,7 @@ public class Loading : MonoBehaviour
             _loadingBar.fillAmount = progressValue;
             yield return null;
         }
+        yield return new WaitForSeconds(delay);
     }
 
 }
