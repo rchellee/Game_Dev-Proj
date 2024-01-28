6ae0b43a9e7a8e6e544f180598873188f02c174c
Miguel <131100404+sanmigs62@users.noreply.github.com>
2024-01-28 21:46:30 +0800

    update bg

--- 2D_BOX/Assets/Scripts/repeatingBG.cs
+++ 2D_BOX/Assets/Scripts/repeatingBG.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RepeatBG : MonoBehaviour
+public class repeatingBG : MonoBehaviour
 {
+
     Vector3 startPos;
-    float repeatWidth;
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider2D>{}.size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - repeatWidth);
+        if(transform.position.x < startPos.x - 15)
         {
             transform.position = startPos;
         }
